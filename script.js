ad2aa613669f183c48b571a810a4827e9374ed64
SURYA-200346 <surya.jsdev@gmail.com>
2025-11-21 01:45:25 +0530

    weather api functionality workes

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
 
-
 const searchbtn = document.getElementById("searchbtn");
 searchbtn.addEventListener("click", () => {
-    const apiKey = "6dae0c680675edcbd02e5e03d95b8a81";
-
     const cityname = document.getElementById("cityname").value.trim();
-    if (cityname==="") {
+    if (cityname === "") {
         alert("please enter a city");
         return
     }
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apiKey}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=6dae0c680675edcbd02e5e03d95b8a81&units=metric`;
     fetch(url)
         .then(res => res.json())
 
         .then(data => {
             if (data.cod != 200) {
                 document.getElementById("weatherResult").innerHTML =
-                    `<p>Error: ${data.message}</p>`;
+                    `<p>Error: ${data.error}</p>`;
                 return;
             }
 
             document.getElementById("weatherResult").innerHTML = `
-                <h3>${data.name}</h3>
-                <p><strong>Temperature:</strong> ${data.main.temp}°C</p>
+            <div class=weatherresult>
+                <h3>City:${data.name}</h3>
+               <p class=temp>  <strong>Temperature:</strong>${data.main.temp}°C</p>
                 <p><strong>Weather:</strong> ${data.weather[0].description}</p>
                 <p><strong>Humidity:</strong> ${data.main.humidity}%</p>
                 <p><strong>Wind:</strong> ${data.wind.speed} m/s</p>
+                </div>
             `;
         })
         .catch(error => {
@@ -33,4 +32,5 @@ searchbtn.addEventListener("click", () => {
                 `<p>Network error</p>`;
             console.error(error);
         });
+
 })
