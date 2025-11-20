f9a6da5d497dd7fb2993fd4c6103f5ba110f38bd
SURYA-200346 <surya.jsdev@gmail.com>
2025-11-21 02:07:20 +0530

    some styles or added

--- script.js
+++ script.js
@@ -2,15 +2,17 @@
 const searchbtn = document.getElementById("searchbtn");
 searchbtn.addEventListener("click", () => {
     const cityname = document.getElementById("cityname").value.trim();
-    if (cityname === "") {
-        alert("please enter a city");
-        return
+    if (!cityname) {
+        document.getElementById("weatherResult").innerHTML =`<p>Please Enter City</p>`;
+        return;
     }
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=6dae0c680675edcbd02e5e03d95b8a81&units=metric`;
     fetch(url)
         .then(res => res.json())
 
         .then(data => {
+            console.log(data);
+
             if (data.cod != 200) {
                 document.getElementById("weatherResult").innerHTML =
                     `<p>Error: ${data.error}</p>`;
